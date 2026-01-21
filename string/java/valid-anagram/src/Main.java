51ed022e87ef8324f79df71b6bef29d08a48d8f2
CoutinhoJhonatas <jhonatas.cjw@outlook.com>
2026-01-21 07:17:33 -0300

    Exercise string valid-anagram

---
+++ string/java/valid-anagram/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("batata", "attaba"));
+        System.out.println(isAnagram("rat", "car"));
+
+        System.out.println("---Alternative method---");
+        System.out.println(isAnagram2("anagram", "nagaram"));
+        System.out.println(isAnagram2("batata", "attaba"));
+        System.out.println(isAnagram2("rat", "car"));
+
+        System.out.println("---Alternative method 2---");
+        System.out.println(isAnagram3("anagram", "nagaram"));
+        System.out.println(isAnagram3("batata", "attaba"));
+        System.out.println(isAnagram3("rat", "car"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
