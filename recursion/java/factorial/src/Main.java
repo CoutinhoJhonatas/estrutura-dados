9d48039d55aa2ccc4805c539dff78468f9b64d79
CoutinhoJhonatas <jhonatas.cjw@outlook.com>
2026-02-25 07:35:23 -0300

    Exercise factorial with tail recursion

--- recursion/java/factorial/src/Main.java
+++ recursion/java/factorial/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Factorial with recursion");
         System.out.println(factorialWithRecursion(0));
         System.out.println(factorialWithRecursion(3));
         System.out.println(factorialWithRecursion(4));
-        System.out.println();
+        System.out.println("Factorial with tail recursion");
+        System.out.println(factorialWithTailRecursion(0));
+        System.out.println(factorialWithTailRecursion(5));
+        System.out.println(factorialWithTailRecursion(4));
+        System.out.println("Factorial without recursion");
         System.out.println(factorialWithoutRecursion(0));
         System.out.println(factorialWithoutRecursion(3));
         System.out.println(factorialWithoutRecursion(4));
@@ -15,6 +20,18 @@ public class Main {
         }
 
         return n * factorialWithRecursion(n - 1);
+    }
+
+    public static int factorialWithTailRecursion(int n) {
+        return factorialTailRecursive(n, 1);
+    }
+
+    public static int factorialTailRecursive(int n, int total) {
+        if (n == 0) {
+            return total;
+        }
+
+        return factorialTailRecursive(n - 1, n * total);
     }
 
     public static int factorialWithoutRecursion(int n) {
--- recursion/javascript/factorial/script.js
+++ recursion/javascript/factorial/script.js
@@ -6,6 +6,20 @@ function factorial(n) {
     return n * factorial(n - 1);
 }
 
+function factorialWithTailRecursion(n) {
+    return factorialTailRecursion(n, 1);
+}
+
+function factorialTailRecursion(n, accumulator) {
+    if (n === 0) {
+        return accumulator;
+    }
+    return factorialTailRecursion(n - 1, n * accumulator);
+}
+
 console.log(factorial(0));
 console.log(factorial(3));
 console.log(factorial(4));
+console.log(factorialWithTailRecursion(0));
+console.log(factorialWithTailRecursion(4));
+console.log(factorialWithTailRecursion(5));
