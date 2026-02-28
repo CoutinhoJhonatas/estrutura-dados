b791b9ba4a3ab629c8ae321c25cdb451ae966db2
CoutinhoJhonatas <jhonatas.cjw@outlook.com>
2026-02-28 12:54:48 -0300

    Exercise recursion power

---
+++ recursion/java/power/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(power(5, 0));
+        System.out.println(power(3, 4));
+        System.out.println(power(5, 10));
+    }
+
+    public static int power(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+
+        return a * power(a, b - 1);
+    }
+}
